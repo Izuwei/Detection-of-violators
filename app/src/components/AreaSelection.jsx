3a6a17a3ee8c1661181ff410cceb9adc62a11d62
Izuwei <sadilekjames@gmail.com>
2022-03-08 20:38:59 +0100

    detection area selection

--- app/src/components/AreaSelection.jsx
+++ app/src/components/AreaSelection.jsx
@@ -1,16 +1,138 @@
-import React, { useContext } from "react";
+import React, { memo, useCallback, useContext, useState } from "react";
+import { Stage, Layer, Rect } from "react-konva";
+import { Button, Typography } from "@mui/material";
 import { DataContext } from "../utils/DataProvider";
+import { useTranslation } from "react-i18next";
 
-const AreaSelection = () => {
-  const { videoThumbnail } = useContext(DataContext);
+const AreaSelection = memo(() => {
+  const { t } = useTranslation();
 
-  console.log("Render: AreaSelection");
-  console.log(videoThumbnail);
-  return (
-    <div>
-      <img alt="temp" src={videoThumbnail} width={200} height={200}></img>
-    </div>
+  const {
+    video,
+    videoThumbnail,
+    reloadVideoThumbnail,
+    areaOfInterest,
+    setupAreaOfInterest,
+  } = useContext(DataContext);
+  console.log(video);
+  const [drawingAOI, setDrawingAOI] = useState([]);
+
+  const handleMouseDown = useCallback(
+    (event) => {
+      if (drawingAOI.length === 0) {
+        const { x, y } = event.target.getStage().getPointerPosition();
+        setDrawingAOI([{ x, y, width: 0, height: 0, key: 0 }]);
+      }
+    },
+    [drawingAOI]
   );
+
+  const handleMouseMove = useCallback(
+    (event) => {
+      if (drawingAOI.length === 1) {
+        const sx = drawingAOI[0].x;
+        const sy = drawingAOI[0].y;
+        const { x, y } = event.target.getStage().getPointerPosition();
+        setupAreaOfInterest([]);
+        setDrawingAOI([
+          {
+            x: sx,
+            y: sy,
+            width: x - sx,
+            height: y - sy,
+            key: 0,
+          },
+        ]);
+      }
+    },
+    [drawingAOI, setupAreaOfInterest]
+  );
+
+  const handleMouseUp = useCallback(
+    (event) => {
+      if (drawingAOI.length === 1) {
+        const sx = drawingAOI[0].x;
+        const sy = drawingAOI[0].y;
+        const { x, y } = event.target.getStage().getPointerPosition();
+        setDrawingAOI([]);
+        setupAreaOfInterest([
+          {
+            sx: sx,
+            sy: sy,
+            ex: x,
+            ey: y,
+          },
+        ]);
+      }
+    },
+    [drawingAOI, setupAreaOfInterest]
+  );
+
+  return (
+    <React.Fragment>
+      {areaOfInterest.length === 1 ? (
+        <Typography align="center" sx={styles.coords}>
+          X:{parseInt(areaOfInterest[0].x / video.thumbnailScale)} Y:
+          {parseInt(areaOfInterest[0].y / video.thumbnailScale)} Width:
+          {parseInt(areaOfInterest[0].width / video.thumbnailScale)} Height:
+          {parseInt(areaOfInterest[0].height / video.thumbnailScale)}
+        </Typography>
+      ) : (
+        <Typography align="center" sx={styles.coords}>
+          X: - Y: - {t("Width")}: - {t("Height")}: -
+        </Typography>
+      )}
+      <Stage
+        width={video.thumbnailWidth}
+        height={video.thumbnailHeight}
+        onMouseDown={handleMouseDown}
+        onMouseMove={handleMouseMove}
+        onMouseUp={handleMouseUp}
+      >
+        <Layer>
+          <Rect
+            width={video.thumbnailWidth}
+            height={video.thumbnailHeight}
+            fillPatternImage={videoThumbnail}
+          />
+          {areaOfInterest.map((value) => {
+            return (
+              <Rect
+                cornerRadius={3}
+                key={value.key}
+                x={value.x}
+                y={value.y}
+                width={value.width}
+                height={value.height}
+                fill="transparent"
+                stroke="orange"
+              />
+            );
+          })}
+          {drawingAOI.map((value) => {
+            return (
+              <Rect
+                key={value.key}
+                x={value.x}
+                y={value.y}
+                width={value.width}
+                height={value.height}
+                fill="transparent"
+                stroke="orange"
+              />
+            );
+          })}
+        </Layer>
+      </Stage>
+      <Button onClick={() => reloadVideoThumbnail(video.url)}>Reload</Button>
+    </React.Fragment>
+  );
+});
+
+const styles = {
+  coords: {
+    fontFamily: "Consolas",
+  },
 };
 
 export default AreaSelection;
--- app/src/utils/DataProvider.js
+++ app/src/utils/DataProvider.js
@@ -11,6 +11,44 @@ export const DataProvider = memo(({ children }) => {
     duration: undefined,
   });
   const [videoThumbnail, setVideoThumbnail] = useState(undefined); // base64 img/png
+  const [detectionArea, setDetectionArea] = useState([]);
+
+  /**
+   * Attention: Coordinates are calculated against the thumbnail, so it is necessary to recalculate them against the scale.
+   */
+  const setupAreaOfInterest = useCallback(
+    (boxes) => {
+      if (boxes.length === 0) {
+        setDetectionArea([]);
+      } else {
+        const sx = parseInt(Math.min(boxes[0].sx, boxes[0].ex)); // Starting point x
+        const sy = parseInt(Math.min(boxes[0].sy, boxes[0].ey)); // Starting point y
+
+        var ex = parseInt(Math.max(boxes[0].sx, boxes[0].ex)); // End point x
+        var ey = parseInt(Math.max(boxes[0].sy, boxes[0].ey)); // End point y
+        ex = ex > video.width ? video.width : ex;
+        ey = ey > video.height ? video.height : ey;
+
+        const width = ex - sx;
+        const height = ey - sy;
+
+        if (width < 50 || height < 50) {
+          setDetectionArea([]);
+        } else {
+          setDetectionArea((prevState) => [
+            {
+              x: sx < 0 ? 0 : sx,
+              y: sy < 0 ? 0 : sy,
+              width: ex - sx,
+              height: ey - sy,
+              key: prevState.length + 1,
+            },
+          ]);
+        }
+      }
+    },
+    [video]
+  );
 
   const reloadVideoThumbnail = useCallback((videoURL) => {
     var tempVideo = document.createElement("video");
@@ -33,7 +71,11 @@ export const DataProvider = memo(({ children }) => {
         var context = canvas.getContext("2d");
         context.drawImage(tempVideo, 0, 0, canvas.width, canvas.height);
         var base64DataImage = canvas.toDataURL("image/png");
-        setVideoThumbnail(base64DataImage);
+        const image = new window.Image();
+        image.src = base64DataImage;
+        image.onload = () => {
+          setVideoThumbnail(image);
+        };
       },
       false
     );
@@ -60,12 +102,20 @@ export const DataProvider = memo(({ children }) => {
           "loadeddata",
           function () {
             reloadVideoThumbnail(videoURL);
+
+            const thumbnailWidth =
+              tempVideo.videoWidth < 900 ? tempVideo.videoWidth : 900;
+            const scale = thumbnailWidth / tempVideo.videoWidth;
+
             setVideo((prevState) => ({
               ...prevState,
               data: accptedFile[0],
               url: videoURL,
               width: tempVideo.videoWidth,
               height: tempVideo.videoHeight,
+              thumbnailWidth: thumbnailWidth,
+              thumbnailHeight: parseInt(tempVideo.videoHeight * scale),
+              thumbnailScale: scale,
               duration: parseInt(tempVideo.duration),
             }));
             console.log("Info: Video uploaded.");
@@ -83,6 +133,9 @@ export const DataProvider = memo(({ children }) => {
         video: video,
         uploadVideo: uploadVideo,
         videoThumbnail: videoThumbnail,
+        reloadVideoThumbnail: reloadVideoThumbnail,
+        areaOfInterest: detectionArea,
+        setupAreaOfInterest: setupAreaOfInterest,
       }}
     >
       {children}
